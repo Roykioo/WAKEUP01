608fd8674b306258bc564c16ed90ed5c062b8813
Cindyxj <13564548694@163.com>
2024-05-02 20:15:46 +0800

    更新

---
+++ Assets/Scripts/Start_Talk.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Talk : MonoBehaviour
+{
+    public int ID;
+    void Start()
+    {
+        DialogeManager.instance.ShowDialogue(ID);
+    }
+
+}
